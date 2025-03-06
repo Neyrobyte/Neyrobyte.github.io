503356691ccef4edd1664e5e3c5a57945a723f6f
Neyrobyte <neyrobyte@gmail.com>
2025-03-06 20:08:31 +0500

    Delete Del directory

--- Del/BrainNotFoundException.java
+++
@@ -1,5 +0,0 @@
-class BrainNotFoundException {
-	public static void main(String[] args) {
-		System.out.println("Error: Could not find or load your brain\nCause: java.lang.BrainNotFoundException: Your brain");
-	}
-}
